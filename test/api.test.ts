7643eff3f77491e216e5f307d98428e680690716
Henriui <henri.uimonen@gmail.com>
2023-10-04 14:42:41 +0300

    tests

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,22 +1,31 @@
-/* import request from 'supertest';
-
+import mongoose from 'mongoose';
+import {registerUser, loginUser} from './userTestFunc';
+import {UserTest} from '../src/interfaces/User';
 import app from '../src/app';
 
-describe('GET /api/v1', () => {
-	it('responds with a json message', (done) => {
-		request(app).get('/api/v1').set('Accept', 'application/json').expect('Content-Type', /json/).expect(
-			200,
-			{
-				message: 'API - 👋🌎🌍🌏',
-			},
-			done
-		);
+describe('Testing user functions', () => {
+	beforeAll(async () => {
+		await mongoose.connect(process.env.DATABASE_URL as string);
 	});
-});
 
-describe('GET /api/v1/emojis', () => {
-	it('responds with a json message', (done) => {
-		request(app).get('/api/v1/emojis').set('Accept', 'application/json').expect('Content-Type', /json/).expect(200, ['😀', '😳', '🙄'], done);
+	afterAll(async () => {
+		await mongoose.connection.close();
+	});
+	const username = 'testuser' + Math.floor(Math.random() * 1000).toString();
+
+	// Create test user
+	const newUser: UserTest = {
+		username: username,
+		email: username + '@testeri.fi',
+		password: 'testo',
+	};
+
+	it('should register a user', async () => {
+		await registerUser(app, newUser);
+	});
+
+	it('should login a user', async () => {
+		const user = await loginUser(app, newUser);
+		console.log('user', user);
 	});
 });
- */
---
+++ test/userTestFunc.ts
@@ -0,0 +1,113 @@
+// eslint-disable-next-line node/no-unpublished-import
+import request from 'supertest';
+import {UserTest} from '../src/interfaces/User';
+
+const registerUser = (url: string | Function, user: UserTest): Promise<UserTest> => {
+	return new Promise((resolve, reject) => {
+		request(url)
+			.post('/graphql')
+			.set('Content-type', 'application/json')
+			.send({
+				query: `mutation regUser($user: RegisterInput!) {
+						registerUser(user: $user) {
+							user {
+								username
+								email
+								password
+							}
+						}
+					}`,
+				variables: {
+					user: {
+						username: user.username,
+						email: user.email,
+						password: user.password,
+					},
+				},
+			})
+			.expect(200, (err, res) => {
+				if (err) {
+					reject(err);
+				}
+				console.log('res.body.data', res.body.data.registerUser.user);
+				resolve(res.body.data.registerUser.user);
+			});
+	});
+};
+
+const getUser = (url: string | Function): Promise<UserTest[]> => {
+	return new Promise((resolve, reject) => {
+		request(url)
+			.get('/graphql')
+			.end((err, res) => {
+				if (err) {
+					reject(err);
+				}
+				resolve(res.body);
+			});
+	});
+};
+
+const loginUser = (url: string | Function, user: UserTest): Promise<UserTest> => {
+	return new Promise((resolve, reject) => {
+		request(url)
+			.post('/graphql')
+			.set('Content-type', 'application/json')
+			.send({
+				query: `mutation LoginUser($credentials: LoginInput!) {
+							loginUser(credentials: $credentials) {
+								user {
+									username
+									email
+									password
+									id
+								}
+								token
+								message
+							}
+						}`,
+				variables: {
+					credentials: {
+						email: user.email,
+						password: user.password,
+					},
+				},
+			})
+			.expect(200, (err, res) => {
+				if (err) {
+					reject(err);
+				}
+				console.log('Login info:', res.body.data.loginUser.user);
+
+				resolve(res.body.data.loginUser);
+			});
+	});
+};
+const deleteUser = (url: string | Function, user: UserTest): Promise<UserTest> => {
+	return new Promise((resolve, reject) => {
+		request(url)
+			.post('/graphql')
+			.set('Content-type', 'application/json')
+			.send({
+				query: `mutation deleteUser {
+						deleteUser {
+							user {
+								id
+							}
+							token
+						}
+					}`,
+				variables: {
+					id: user.id,
+				},
+			})
+			.expect(200, (err, res) => {
+				if (err) {
+					reject(err);
+				}
+				resolve(res.body.data.deleteUser.user);
+			});
+	});
+};
+
+export {registerUser, loginUser, deleteUser, getUser};
