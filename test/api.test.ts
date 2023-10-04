d3147303ce3fad8f92338f29631892fd2c786fa0
Henriui <henri.uimonen@gmail.com>
2023-10-04 15:04:20 +0300

    create and login workie now

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
-import {registerUser, loginUser} from './userTestFunc';
+import {registerUser, loginUser, deleteUser, getUser} from './userTestFunc';
 import {UserTest} from '../src/interfaces/User';
 import app from '../src/app';
 
 describe('Testing user functions', () => {
+	// Connections
+
 	beforeAll(async () => {
 		await mongoose.connect(process.env.DATABASE_URL as string);
 	});
@@ -11,21 +13,31 @@ describe('Testing user functions', () => {
 	afterAll(async () => {
 		await mongoose.connection.close();
 	});
+
+	// Create random username incase test fails and we lazy to delete user
+
 	const username = 'testuser' + Math.floor(Math.random() * 1000).toString();
 
 	// Create test user
+
 	const newUser: UserTest = {
 		username: username,
 		email: username + '@testeri.fi',
 		password: 'testo',
 	};
 
+	// User to be used in tests
+	let user: UserTest;
+
 	it('should register a user', async () => {
 		await registerUser(app, newUser);
 	});
 
 	it('should login a user', async () => {
-		const user = await loginUser(app, newUser);
-		console.log('user', user);
+		user = await loginUser(app, newUser);
+	});
+
+	it('should delete a user', async () => {
+		await deleteUser(app, user);
 	});
 });
