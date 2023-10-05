cda16ac0caf39880b4da367df1cf094b12360a41
Henriui <henri.uimonen@gmail.com>
2023-10-05 16:05:11 +0300

    message tests

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,54 +1,99 @@
-import mongoose from 'mongoose';
-import {UserTest} from '../src/interfaces/User';
+import mongoose, {mongo} from 'mongoose';
 import app from '../src/app';
 import LoginMessageResponse from '../src/interfaces/LoginMessageResponse';
 
-// User test functions
+// Interfaces
+import {UserTest} from '../src/interfaces/User';
+import {MessageTest} from '../src/interfaces/Message';
 
+// Test functions
 import {registerUser, loginUser, deleteUser, getUsers, getUserById} from './userTestFunc';
+import {createMessage, deleteMessage, messageById, messageBySender} from './messageTestFunc';
 
-describe('Testing user functions', () => {
-	// Connections
+// Connections
 
-	beforeAll(async () => {
-		await mongoose.connect(process.env.DATABASE_URL as string);
-	});
+beforeAll(async () => {
+	await mongoose.connect(process.env.DATABASE_URL as string);
+});
 
-	afterAll(async () => {
-		await mongoose.connection.close();
-	});
+afterAll(async () => {
+	await mongoose.connection.close();
+});
 
-	// Create random username incase test fails and we lazy to delete user
+describe('Testing backend functions', () => {
+	// Reg and login before all tests
+
+	// Create random username user incase test fails and we lazy to delete user
 
 	const username = 'testuser' + Math.floor(Math.random() * 1000).toString();
-
-	// Create test user
-
 	const newUser: UserTest = {
 		username: username,
 		email: username + '@testeri.fi',
 		password: 'testo' + username,
 	};
 
-	// User to be used in tests
-	let userData: LoginMessageResponse;
+	// Userdata for tokens etc.
 
-	it('should register a user', async () => {
-		await registerUser(app, newUser);
+	var userData: LoginMessageResponse;
+
+	describe('Testing user reg/login', () => {
+		it('should register a user', async () => {
+			await registerUser(app, newUser);
+		});
+
+		it('should login a user', async () => {
+			// Preserve userdata for later tests (token needed)
+			userData = await loginUser(app, newUser);
+		});
 	});
 
-	it('should login a user', async () => {
-		userData = await loginUser(app, newUser);
-	});
-	it('should get users', async () => {
-		await getUsers(app, userData);
+	describe('Testing user functions', () => {
+		// User to be used in tests
+
+		it('should get users', async () => {
+			await getUsers(app, userData);
+		});
+
+		it('should get a user by id', async () => {
+			await getUserById(app, userData.user.id);
+		});
 	});
 
-	it('should get a user by id', async () => {
-		await getUserById(app, userData.user.id);
+	// Tests for messages
+
+	describe('Testing message functions', () => {
+		let testMessage : MessageTest;
+		it('should create a message', async () => {
+			// Test message we input
+			const message: MessageTest = {
+				date: new Date(),
+				content: 'test message',
+				sender: userData.user.id as unknown as mongoose.Types.ObjectId,
+			};
+
+			testMessage = await createMessage(app, userData, message) as MessageTest;
+		});
+
+		it('should get a message by id', async () => {
+			await messageById(app, testMessage.id as string);
+		});
+
+		it('should get messages by sender', async () => {
+			await messageBySender(app, userData.user.id as string)
+		});
+
+		it('should delete a message', async () => {
+			await deleteMessage(app, userData, testMessage.id as string);
+		});
+
 	});
 
-	it('should delete a user', async () => {
-		await deleteUser(app, userData);
+	// Deletion test ( For user )
+
+	describe('User can be deleted', () => {
+		it('should delete a user', async () => {
+			await deleteUser(app, userData);
+		});
 	});
 });
+
---
+++ test/messageTestFunc.ts
@@ -0,0 +1,189 @@
+import request from 'supertest';
+import {MessageTest} from '../src/interfaces/Message';
+import LoginMessageResponse from '../src/interfaces/LoginMessageResponse';
+import mongoose, {mongo} from 'mongoose';
+
+const createMessage = async (url: string | Function, userData: LoginMessageResponse, testMessage: MessageTest) => {
+	return new Promise((resolve, reject) => {
+		request(url)
+			.post('/graphql')
+			.set('Content-type', 'application/json')
+			.send({
+				query: `
+            mutation CreateMessage($message: MessageInput!, $user: UserWithTokenInput!) {
+                createMessage(message: $message, user: $user) {
+                  id
+                  date
+                  content
+                  sender {
+                    id
+                    username
+                    email
+                    password
+                    description
+                    avatar
+                    lastLogin
+                    role
+                  }
+                }
+              }
+              
+            `,
+				variables: {
+					message: testMessage,
+					user: {
+						id: userData.user.id,
+						token: userData.token,
+					},
+				},
+			})
+			.expect(200, (err, res) => {
+				if (err) {
+					reject(err);
+				}
+				const message = res.body.data.createMessage;
+				expect(message).toHaveProperty('id');
+				expect(message).toHaveProperty('date');
+				expect(message).toHaveProperty('content');
+				expect(message).toHaveProperty('sender');
+				expect(message.content).toBe(testMessage.content);
+				expect(message.sender.id).toBe(userData.user.id);
+				resolve(message);
+			});
+	});
+};
+
+const messageById = async (url: string | Function, messageId: string) => {
+	return new Promise((resolve, reject) => {
+		request(url)
+			.post('/graphql')
+			.set('Content-type', 'application/json')
+			.send({
+				query: `
+            query MessageById($messageByIdId: ID!) {
+                messageById(id: $messageByIdId) {
+                  id
+                  date
+                  content
+                  sender {
+                    id
+                    username
+                    email
+                    password
+                    description
+                    avatar
+                    lastLogin
+                    role
+                  }
+                }
+              } 
+            `,
+				variables: {
+					messageByIdId: messageId,
+				},
+			})
+			.expect(200, (err, res) => {
+				if (err) {
+					reject(err);
+				}
+				const message = res.body.data.messageById;
+				expect(message).toHaveProperty('id');
+				expect(message).toHaveProperty('date');
+				expect(message).toHaveProperty('content');
+				expect(message).toHaveProperty('sender');
+				resolve(message);
+			});
+	});
+};
+
+const messageBySender = async (url: string | Function, userId: string) => {
+    return new Promise((resolve, reject) => {
+        request(url)
+            .post('/graphql')
+            .set('Content-type', 'application/json')
+            .send({
+                query: `
+                query MessagesBySender($sender: ID!) {
+                    messagesBySender(sender: $sender) {
+                      id
+                      date
+                      content
+                      sender {
+                        id
+                        username
+                        email
+                        password
+                        description
+                        avatar
+                        lastLogin
+                        role
+                      }
+                    }
+                  } 
+            `,
+                variables: {
+                    sender: userId,
+                },
+            })
+            .expect(200, (err, res) => {
+                if (err) {
+                    reject(err);
+                }
+                const message = res.body;
+                console.log('message', message)
+                resolve(message);
+            });
+    });
+}
+
+const deleteMessage = async (url: string | Function, userData: LoginMessageResponse, messageId: string) => {
+	return new Promise((resolve, reject) => {
+		request(url)
+			.post('/graphql')
+			.set('Content-type', 'application/json')
+			.send({
+				query: `
+            mutation DeleteMessage($deleteMessageId: ID!, $user: UserWithTokenInput) {
+                deleteMessage(id: $deleteMessageId, user: $user) {
+                  id
+                  date
+                  content
+                  sender {
+                    id
+                    username
+                    email
+                    password
+                    description
+                    avatar
+                    lastLogin
+                    role
+                  }
+                }
+              }
+            `,
+				variables: {
+					deleteMessageId: messageId as unknown as mongoose.Types.ObjectId,
+					user: {
+						id: userData.user.id as unknown as mongoose.Types.ObjectId,
+						token: userData.token,
+					},
+				},
+			})
+			.expect(200, (err, res) => {
+				if (err) {
+					reject(err);
+				}
+				const message = res.body.data.deleteMessage;
+				expect(message).toHaveProperty('id');
+				expect(message.id).toBe(messageId);
+				expect(message).toHaveProperty('date');
+				expect(message).toHaveProperty('content');
+				expect(message).toHaveProperty('sender');
+				expect(message.sender.id).toBe(userData.user.id);
+				resolve(message);
+			});
+	});
+};
+
+export {createMessage, deleteMessage, messageById, messageBySender};
+
