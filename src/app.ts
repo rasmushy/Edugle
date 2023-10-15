9197eea23f9cdf8cb856f18f4993261e18cc7501
lassib <91030337+lassib@users.noreply.github.com>
2023-10-15 23:15:19 +0300

    fix to limitters

--- src/app.ts
+++ src/app.ts
@@ -37,8 +37,8 @@ const wsServer = new WebSocketServer({
 			Mutation: {
 				loginUser: rateLimitRule({window: '1s', max: 5}),
 				initiateChat: rateLimitRule({window: '1s', max: 5}),
-				sendMessage: rateLimitRule({window: '1s', max: 5}),
-				createUser: rateLimitRule({window: '1s', max: 5}),
+				createMessage: rateLimitRule({window: '1s', max: 5}),
+				registerUser: rateLimitRule({window: '1s', max: 5}),
 				joinChat: rateLimitRule({window: '1s', max: 5}),
 				leaveChat: rateLimitRule({window: '1s', max: 5}),
 			},
